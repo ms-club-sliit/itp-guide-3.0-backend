337afe4fb4abd04ec19c3403236f40d1274b9e96
ShivaaniDushya <it20006884@my.sliit.lk>
2022-01-03 22:35:09 +0530

    Created Todo Model, Todo Controller, Todo Service and Todo Repository

--- src/main/java/com/example/demo/itpguidebackend/todos/TodoRepository.java
+++ src/main/java/com/example/demo/itpguidebackend/todos/TodoRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.itpguidebackend.todos;
 
-public class TodoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+
 }
--- src/main/java/com/example/demo/itpguidebackend/todos/TodoService.java
+++ src/main/java/com/example/demo/itpguidebackend/todos/TodoService.java
@@ -1,4 +1,38 @@
 package com.example.demo.itpguidebackend.todos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TodoService {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> getTodos() {
+        return todoRepository.findAll();
+    }
+
+    public String addTodo(Todo todo) {
+        todoRepository.save(todo);
+        return "Todo with id " + todo.getId() + " successfully saved.";
+    }
+
+    public String deleteTodo(int id) {
+        todoRepository.deleteById(id);
+        return "Todo with id " + id + " successfully deleted.";
+    }
+
+    public String updateTodo(Todo todo) {
+        Todo existingTodo = todoRepository.findById(todo.getId()).orElse(null);
+        existingTodo.setDescription(todo.getDescription());
+        existingTodo.setCategory(todo.getCategory());
+        existingTodo.setDueDate(todo.getDueDate());
+        todoRepository.save(existingTodo);
+
+        return "Todo with id " + todo.getId() + " successfully updated.";
+    }
+
 }
