7983245aa4fb41e0cd6dcc1664904fd7f9be78b8
ShivaaniDushya <it20006884@my.sliit.lk>
2022-01-03 21:41:36 +0530

    Created users folder with classes

---
+++ src/main/java/com/example/demo/itpguidebackend/todos/TodoRepository.java
@@ -0,0 +1,4 @@
+package com.example.demo.itpguidebackend.todos;
+
+public class TodoRepository {
+}
--- src/main/java/com/example/demo/itpguidebackend/users/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.itpguidebackend.users;
-
-public class UserController {
-}
