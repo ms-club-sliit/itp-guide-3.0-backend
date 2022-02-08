898a80a86c7511e543efa5404a85b43d671f93b4
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2022-02-08 20:49:49 +0530

    Mongodb connected

--- src/main/java/com/example/demo/itpguidebackend/todos/repositories/TodoRepository.java
+++ src/main/java/com/example/demo/itpguidebackend/todos/repositories/TodoRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.itpguidebackend.todos.repositories;
 
 import com.example.demo.itpguidebackend.todos.models.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TodoRepository extends JpaRepository<Todo, Integer> {
+public interface TodoRepository extends MongoRepository<Todo, Integer> {
 
 }
